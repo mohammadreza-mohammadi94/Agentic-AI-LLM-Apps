2f3d9d24bf88cb62c52f218189872d6fe41cf42b
mohammadreza-mohammadi94 <mr.mhmdi93@gmail.com>
2025-07-24 11:04:41 +0330

    add agents prompt

---
+++ Data Extractor Agent/prompts.py
@@ -0,0 +1,7 @@
+EXTRACTOR_SYSTEM_PROMPT = """
+Your are a world-class data extraction expert.
+Your task is to carefully read the user-provided text describing a project
+and extract the required information into the specified format.
+Identify the project's official name, the list of technologies used and 
+the primary objective or goal of the project.
+"""
